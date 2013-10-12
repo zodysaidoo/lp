d750fbb7ee0b35a12f40b1278bfaa2a3428b11ba
Zeid Nidal Ali <zeid@gmx.de>
2013-10-12 12:15:30 +0200

    Commit vor merge

--- src/Lernparadies/LernparadiesBenutzerBundle/DataFixtures/ORM/LoadBenutzergruppeData.php
+++ src/Lernparadies/LernparadiesBenutzerBundle/DataFixtures/ORM/LoadBenutzergruppeData.php
@@ -35,35 +35,46 @@ class LoadBenutzergruppeData extends AbstractFixture implements OrderedFixtureIn
 	{
         $userManager = $this->container->get('fos_user.group_manager');
 
+        /** @var Benutzergruppe $benutzergruppeAdmin */
         $benutzergruppeAdmin = $userManager->createGroup('Administrator');
         $benutzergruppeAdmin->addRole('ROLE_ADMIN');
+        $benutzergruppeAdmin->addRole('ROLE_USER');
+        $benutzergruppeAdmin->addRole('ROLE_SONATA_ADMIN');
+        $benutzergruppeAdmin->addRole('ROLE_ALLOWED_TO_SWITCH');
         $em->persist($benutzergruppeAdmin);
 
+        /** @var Benutzergruppe $benutzergruppeFreeUser */
         $benutzergruppeFreeUser = $userManager->createGroup('Freier User');
         $benutzergruppeFreeUser->addRole('ROLE_FREE_USER');
         $em->persist($benutzergruppeFreeUser);
 
+        /** @var Benutzergruppe $benutzergruppeLehrer */
         $benutzergruppeLehrer = $userManager->createGroup('Lehrer');
         $benutzergruppeLehrer->addRole('ROLE_TEACHER');
         $em->persist($benutzergruppeLehrer);
 
+        /** @var Benutzergruppe $benutzergruppeSchueler */
         $benutzergruppeSchueler = $userManager->createGroup('Schüler');
         $benutzergruppeSchueler->addRole('ROLE_STUDENT');
         $em->persist($benutzergruppeSchueler);
 
+        /** @var Benutzergruppe $benutzergruppeSchulleitung */
         $benutzergruppeSchulleitung = $userManager->createGroup('Schulleitung');
         $benutzergruppeSchulleitung->addRole('ROLE_HEADTEACHER');
         $benutzergruppeSchulleitung->addRole('ROLE_TEACHER');
         $em->persist($benutzergruppeSchulleitung);
 
+        /** @var Benutzergruppe $benutzergruppeRedakteur */
         $benutzergruppeRedakteur = $userManager->createGroup('Redakteur');
-		$benutzergruppeRedakteur->addRole('ROLE_REDAKTEUR');
-		$em->persist($benutzergruppeRedakteur);
+        $benutzergruppeRedakteur->addRole('ROLE_REDAKTEUR');
+        $em->persist($benutzergruppeRedakteur);
 
+        /** @var Benutzergruppe $benutzergruppeEltern */
         $benutzergruppeEltern = $userManager->createGroup('Eltern');
-		$benutzergruppeEltern->addRole('ROLE_PARENT');
-		$em->persist($benutzergruppeEltern);
+        $benutzergruppeEltern->addRole('ROLE_PARENT');
+        $em->persist($benutzergruppeEltern);
 
+        /** @var Benutzergruppe $benutzergruppeMinisteriumsmitarbeiter */
         $benutzergruppeMinisteriumsmitarbeiter = $userManager->createGroup('Ministeriumsmitarbeiter');
 		$benutzergruppeMinisteriumsmitarbeiter->addRole('ROLE_GOUVERNMENT_EMPLOYEE');
 		$em->persist($benutzergruppeMinisteriumsmitarbeiter);
