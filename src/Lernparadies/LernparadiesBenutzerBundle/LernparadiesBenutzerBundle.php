ebc542ab7f2b015abf534752709168c7a6e6ff2c
Zeid Nidal Ali <zeid@gmx.de>
2013-10-12 10:12:52 +0200

    Commit BenutzerBundle vor dem EasyExtends

--- src/Lernparadies/LernparadiesBenutzerBundle/LernparadiesBenutzerBundle.php
+++ src/Lernparadies/LernparadiesBenutzerBundle/LernparadiesBenutzerBundle.php
@@ -6,8 +6,10 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LernparadiesBenutzerBundle extends Bundle
 {
+    /**
     public function getParent()
     {
         return 'FOSUserBundle';
     }
+    /**/
 }
