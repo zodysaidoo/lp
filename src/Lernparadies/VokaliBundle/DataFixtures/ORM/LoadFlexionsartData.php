393b56b6c6cbd7d64676b9d14389cda8741b6d93
Zeid Nidal Ali <zeid@gmx.de>
2013-11-02 21:28:31 +0100

    Verbflexionen der Gegenwart hinzugefügt

--- src/Lernparadies/VokaliBundle/DataFixtures/ORM/LoadFlexionsartData.php
+++ src/Lernparadies/VokaliBundle/DataFixtures/ORM/LoadFlexionsartData.php
@@ -22,8 +22,44 @@ class LoadFlexionsartData extends AbstractFixture implements OrderedFixtureInter
 		$verbInfinitv = new Flexionsart();
         $verbInfinitv->setName('Verb Infinitiv');
         $verbInfinitv->setCode('Verb Infinitiv');
-        $verbInfinitv->setComment('Verb als Infinitiv (ungebeugt)');
+        $verbInfinitv->setComment('Verb als Infinitiv (ungebeugt) z.B. laufen');
 		$em->persist($verbInfinitv);
+
+		$verbErstePersonSingular = new Flexionsart();
+        $verbErstePersonSingular->setName('Verbform 1. Person Singular');
+        $verbErstePersonSingular->setCode('Verbform erste Person Singular');
+        $verbErstePersonSingular->setComment('gebeugte Verbform der ersten Person Singular z.B. ich laufe');
+		$em->persist($verbErstePersonSingular);
+
+		$verbZweitePersonSingular = new Flexionsart();
+        $verbZweitePersonSingular->setName('Verbform 2. Person Singular');
+        $verbZweitePersonSingular->setCode('Verbform zweite Person Singular');
+        $verbZweitePersonSingular->setComment('gebeugte Verbform der zweiten Person Singular z.B. du läufst');
+		$em->persist($verbZweitePersonSingular);
+
+		$verbDrittePersonSingular = new Flexionsart();
+        $verbDrittePersonSingular->setName('Verbform 3. Person Singular');
+        $verbDrittePersonSingular->setCode('Verbform dritte Person Singular');
+        $verbDrittePersonSingular->setComment('gebeugte Verbform der dritten Person Singular z.B. es läuft');
+		$em->persist($verbDrittePersonSingular);
+
+		$verbErstePersonPlural = new Flexionsart();
+        $verbErstePersonPlural->setName('Verbform 1. Person Plural');
+        $verbErstePersonPlural->setCode('Verbform erste Person Plural');
+        $verbErstePersonPlural->setComment('gebeugte Verbform der ersten Person Plural z.B. wir laufen');
+		$em->persist($verbErstePersonPlural);
+
+		$verbZweitePersonPlural = new Flexionsart();
+        $verbZweitePersonPlural->setName('Verbform 2. Person Plural');
+        $verbZweitePersonPlural->setCode('Verbform zweite Person Plural');
+        $verbZweitePersonPlural->setComment('gebeugte Verbform der zweiten Person Plural z.B. ihr lauft');
+		$em->persist($verbZweitePersonPlural);
+
+		$verbDrittePersonPlural = new Flexionsart();
+        $verbDrittePersonPlural->setName('Verbform 3. Person Plural');
+        $verbDrittePersonPlural->setCode('Verbform dritte Person Plural');
+        $verbDrittePersonPlural->setComment('gebeugte Verbform der dritten Person Plural z.B. sie laufen');
+		$em->persist($verbDrittePersonPlural);
 
 		$nomenNominativ = new Flexionsart();
         $nomenNominativ->setName('Nomen Nominativ');
